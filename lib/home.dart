2d21f0815be2dd74da96fa025d4c32243d856fc5
Ashraful Islam Akik <ashrafulislam3195@gmail.com>
2025-09-08 12:54:16 +0600

    Updated features

--- lib/home.dart
+++ lib/home.dart
@@ -1,19 +1,15 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:flutter_batchapp/widget/framewidget.dart';
 
 class home extends StatelessWidget {
   const home({super.key});
-=======
 
-class Home extends StatelessWidget {
-  const Home({super.key});
->>>>>>> be8e462486c5ca3800a6d2a821b03948e698d493
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-<<<<<<< HEAD
+
       
       appBar: AppBar(
         title: Text("Home"),
@@ -33,86 +29,4 @@ class Home extends StatelessWidget {
 }
 
 
-=======
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-        title: Text("Ashraful",),
-        centerTitle: true,
-      ),
-      body:
-      SingleChildScrollView(
-        child: Column ( children: [
 
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-
-              children: [
-                Text(" Ashraful ",
-                  style: TextStyle( fontSize: 30, color: Colors.blue),),
-                Text(" Ashraful ",
-                  style: TextStyle( fontSize: 30, color: Colors.blue),),
-                Text(" Ashraful ",
-                  style: TextStyle( fontSize: 30, color: Colors.blue),),
-                Text(" Ashraful ",
-                  style: TextStyle( fontSize: 30, color: Colors.blue),),
-                Text(" Ashraful ",
-                  style: TextStyle( fontSize: 30, color: Colors.blue),),Text(" Ashraful ",
-                  style: TextStyle( fontSize: 30, color: Colors.blue),),
-                Text(" Ashraful ",
-                  style: TextStyle( fontSize: 30, color: Colors.blue),),
-                Text(" Ashraful ",
-                  style: TextStyle( fontSize: 30, color: Colors.blue),),
-                Text(" Ashraful ",
-                  style: TextStyle( fontSize: 30, color: Colors.blue),),
-                Text(" Ashraful ",
-                  style: TextStyle( fontSize: 30, color: Colors.blue),),
-                Text(" Ashraful ",
-                  style: TextStyle( fontSize: 30, color: Colors.blue),),Text(" Ashraful ",
-                  style: TextStyle( fontSize: 30, color: Colors.blue),),
-              ],
-
-            ),
-          ),
-
-
-          Text(" Ashraful ",
-          style: TextStyle( fontSize: 30, color: Colors.blue),),
-          Text(" Ashraful ",
-            style: TextStyle( fontSize: 30, color: Colors.blue),),
-          Text(" Ashraful ",
-            style: TextStyle( fontSize: 30, color: Colors.blue),),
-          Text(" Ashraful ",
-            style: TextStyle( fontSize: 30, color: Colors.blue),),
-          Text(" Ashraful ",
-            style: TextStyle( fontSize: 30, color: Colors.blue),),Text(" Ashraful ",
-            style: TextStyle( fontSize: 30, color: Colors.blue),),
-          Text(" Ashraful ",
-            style: TextStyle( fontSize: 30, color: Colors.blue),),
-          Text(" Ashraful ",
-            style: TextStyle( fontSize: 30, color: Colors.blue),),
-          Text(" Ashraful ",
-            style: TextStyle( fontSize: 30, color: Colors.blue),),
-          Text(" Ashraful ",
-            style: TextStyle( fontSize: 30, color: Colors.blue),),
-          Text(" Ashraful ",
-            style: TextStyle( fontSize: 30, color: Colors.blue),),
-          Text(" Ashraful ",
-            style: TextStyle( fontSize: 30, color: Colors.blue),),
-          Text(" Ashraful ",
-            style: TextStyle( fontSize: 30, color: Colors.blue),),
-        ],
-        
-        ),
-      ),
-
-      floatingActionButton: FloatingActionButton(onPressed:
-
-          (){
-        print("Clik");
-      },
-      child: Icon(Icons.add),),
-    );
-  }
-}
->>>>>>> be8e462486c5ca3800a6d2a821b03948e698d493
